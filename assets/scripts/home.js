60f76296fa43d345e8d8ce3d507f13b43679b3de
Stanleycordeiro <stanley.cordeiro@outlook.com.br>
2023-07-02 20:29:02 -0300

    navegação entre telas e tela de Entrada e saida de gastos add

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,9 +1,33 @@
+// ELEMENTOS HOME
+form = {
+  mainScreen: () => document.getElementById("main"),
+  homeScreen: () => document.getElementById("mainHome"),
+  enterAndBackScreen: () =>
+    document.getElementById("mainHomeEnterAndBackMoney"),
+};
+
 function logout() {
-    showLoading();
-    firebase.auth().signOut().then(() => {
-        hideLoading();
-        window.location.href = "../../index.html";
-    }).catch(() => {
-        alert("Erro ao fazer logout");
+  showLoading();
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      hideLoading();
+      window.location.href = "../../index.html";
     })
+    .catch(() => {
+      alert("Erro ao fazer logout");
+    });
+}
+
+
+// FUNÇÕES TROCAR TELA
+function toggleScreenHome() {
+    form.homeScreen().style.display = "block";
+    form.enterAndBackScreen().style.display = "none";    
+}
+
+function toggleScreenCash() {
+    form.homeScreen().style.display = "none";
+    form.enterAndBackScreen().style.display = "block";   
 }
