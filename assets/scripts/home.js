d5d40ffc4a7af633754ffccae593a40491b87019
Stanleycordeiro <stanley.cordeiro@outlook.com.br>
2023-07-03 20:02:11 -0300

    Chamando e criando elementos que estão no DB

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -20,20 +20,96 @@ function logout() {
     });
 }
 
-
 // FUNÇÕES TROCAR TELA
 function toggleScreenHome() {
-    form.homeScreen().style.display = "block";
-    form.enterAndBackScreen().style.display = "none";    
+  form.homeScreen().style.display = "block";
+  form.enterAndBackScreen().style.display = "none";
 }
 
 function toggleScreenCash() {
-    form.homeScreen().style.display = "none";
-    form.enterAndBackScreen().style.display = "block";   
+  form.homeScreen().style.display = "none";
+  form.enterAndBackScreen().style.display = "block";
 }
 
 function monitorScreenInit() {
-    form.homeScreen().style.display = "block";
-    form.enterAndBackScreen().style.display = "none";
+  form.homeScreen().style.display = "block";
+  form.enterAndBackScreen().style.display = "none";
 }
 monitorScreenInit();
+
+//chamada ao banco
+findTransations();
+
+function findTransations() {
+  setTimeout(() => {
+    addTransactionsScreen(fakeTransactions);
+  }, 1000);
+}
+
+//Printar elemento na tela vindo do banco
+function addTransactionsScreen(transactions) {
+  const orderedList = document.getElementById('transactions');
+
+  transactions.forEach(transactions => {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const b = document.createElement('b');
+    const pValue = document.createElement('p');
+    const pTransctionType = document.createElement('p');
+
+    div.setAttribute("id", "divLi");
+    div.classList.add("card", "list-group-item", "shadow-sm", "expense");
+    div.classList.add(transactions.type);
+    b.innerHTML = formatDate(transactions.date);
+    pValue.innerHTML = formatMoney(transactions.money);
+    pTransctionType.innerHTML = transactions.transctionType;
+
+    orderedList.appendChild(li);
+    li.appendChild(div);
+    div.appendChild(b);
+    div.appendChild(pValue);
+    div.appendChild(pTransctionType);
+  });
+}
+
+//função para formatação dos campos do li
+function formatDate (date) {
+  return new Date(date).toLocaleDateString('pt-br');
+}
+function formatMoney (money) {
+  return `${"R$ "} ${money.toFixed(2)}`;
+}
+
+//banco fake
+const fakeTransactions = [
+  {
+    type: "expense",
+    date: "2023-07-02",
+    money: 20,
+    transctionType: "Supermercado",
+  },
+  {
+    type: "income",
+    date: "2023-06-22",
+    money: 1200,
+    transctionType: "Salario",
+  },
+  {
+    type: "expense",
+    date: "2023-06-20",
+    money: 200,
+    transctionType: "mecanica",
+  },
+  {
+    type: "income",
+    date: "2023-06-15",
+    money: 300,
+    transctionType: "Remuneração variavel",
+  },
+  {
+    type: "income",
+    date: "2023-06-02",
+    money: 210,
+    transctionType: "Remuneração variavel",
+  },
+];
