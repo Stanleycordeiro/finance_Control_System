9c0ae412a7bc251bfcece7022ccb58f12ac2b1fb
Stanleycordeiro <stanley.cordeiro@outlook.com.br>
2023-07-12 19:20:22 -0300

    inclusão do firebase-admin e resgatando transaction GET REST

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 import express from "express";
+import admin from "firebase-admin";
 
 const app = express();
 
+admin.initializeApp({
+  credential: admin.credential.cert("serviceAcountKey.json"),
+});
+
 app.get("/transactions", (request, response) => {
   console.log("GET transactions");
-  response.json([{ id: 1 }]);
+  admin
+    .firestore()
+    .collection("transactions")
+    .get()
+    .then(snapshot => {
+      const transactions = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        uid: doc.id
+      }));
+      response.json(transactions);
+    });
+  
 });
 
 app.listen(3000, () => console.log("API iniciado no localhost:3000"));
